45fb36e2416afd0d64e5fc6da7ff18e82235947a
Pervonachalo <75913595+Pervonachalo@users.noreply.github.com>
2026-01-17 22:57:22 +0500

    Add comments to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,11 @@
+-- 1. Лидер-клавиши (должны быть объявлены ПЕРЕД загрузкой плагинов)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+-- vim.g.maplocalleader = " "
 
-require("core.options")
-require("core.keymaps")
-require("core.lazy")
+-- 2. Подключение модулей ядра
+require("core.options") -- Сначала базовые опции
+require("core.keymaps") -- Потом горячие клавиши
+
+-- 3. Инициализация менеджера плагинов
+require("core.lazy") -- И в конце загрузка плагинов
